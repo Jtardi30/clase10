3ed37fa0c879def890263fc77b0d1115832e42e8
Jtardi30 <tardi.joel@gmail.com>
2022-08-28 17:19:52 -0300

    Terminando la clase

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express"); 
+const path = require("path"); 
 const app = express(); 
-app.get("/", (req, res) =>{
-    res.send("Bienvenidos a mi Mundo"); 
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html")); 
 }); 
-app.listen(3000, () =>
+app.listen(3030, () =>
 console.log("Servidor Corriendo")); 
 
